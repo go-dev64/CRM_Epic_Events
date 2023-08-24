1455404a6b75f962a6c1245c319a80363435d9d2
Oblin Guillaume <guillaume.oblin@sfr.fr>
2023-08-24 15:05:20 +0200

    add query_db.py/ test_database_connection passed

--- tests/unit_tests/test_db.py
+++ tests/unit_tests/test_db.py
@@ -1,12 +1,13 @@
 import pytest
-from sqlalchemy import create_engine
+import app.query_db as query_db
 
 
-def test_database_connection():
-    # test try to connect to database
-    try:
-        engine = database_engine()
-        connection = engine.connect()
-        assert connection is not None
-    except Exception as e:
-        pytest.fail(f"Échec de la connexion à la base de données : {e}")
+class TestDatabase:
+    def test_database_connection(self):
+        # test try to connect to database
+        try:
+            engine = query_db.database_engine()
+            connection = engine.connect()
+            assert connection is not None
+        except Exception as e:
+            pytest.fail(f"Échec de la connexion à la base de données : {e}")
