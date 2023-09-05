d39a1e21b7ded307054978916a9187bf72f26dcb
Oblin Guillaume <guillaume.oblin@sfr.fr>
2023-09-05 14:53:51 +0200

    test_find_user_with_wrong_email passed / refacoring: new private_func > _crete_user

--- tests/user_tests/unit_test/test_login.py
+++ tests/user_tests/unit_test/test_login.py
@@ -4,7 +4,11 @@ from crm_app.user.models.users import Manager, Seller, Supporter
 from crm_app.user.models.authentiction import Authentication
 
 
-class TestLogin:
+class TestAuthentication:
+    def _create_users(self, db_session, users):
+        db_session.add_all(users)
+        db_session.commit()
+
     @pytest.mark.parametrize(
         "email, user_name",
         [
@@ -14,15 +18,13 @@ class TestLogin:
         ],
     )
     def test_find_user_with_right_email(self, db_session, users, email, user_name):
-        db_session.add_all(users)
-        db_session.commit()
+        self._create_users(db_session, users)
         auth = Authentication(db_session)
         user = auth.find_user_with_email(email=email)
         assert user.name == user_name
 
-    def test_find_user_with_wrong(self, db_session, users):
-        db_session.add_all(users)
-        db_session.commit()
+    def test_find_user_with_wrong_email(self, db_session, users):
+        self._create_users(db_session, users)
         auth = Authentication(db_session)
         user = auth.find_user_with_email(email="toto@gmail.fr")
         assert user == None
