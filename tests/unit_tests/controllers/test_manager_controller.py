5fffb193d1d6eb9767e999c2e0d1015e4d3862a0
Oblin Guillaume <guillaume.oblin@sfr.fr>
2023-09-20 16:09:21 +0200

    new test: testtest_create_new_element

--- tests/unit_tests/controllers/test_manager_controller.py
+++ tests/unit_tests/controllers/test_manager_controller.py
@@ -25,3 +25,22 @@ class TestManagerController:
                 assert manager_ctrl.create_new_element(session=session) == "create_new_contract"
             else:
                 pass
+
+    @pytest.mark.parametrize("department", [(1), (2), (3)])
+    def test_create_new_user(self, db_session, users, current_user_is_manager, mocker, department):
+        with db_session as session:
+            users
+            current_user_is_manager
+            manager_ctrl = ManagerController()
+
+            mocker.patch("crm.view.generic_view.GenericView.select_element_view", return_value=department)
+            mocker.patch("crm.models.users.Manager.create_new_manager", return_value="new_manager")
+            mocker.patch("crm.models.users.Manager.create_new_seller", return_value="new_seller")
+            mocker.patch("crm.models.users.Manager.create_new_supporter", return_value="new_supporter")
+
+            if department == 1:
+                assert manager_ctrl.create_new_user(session=session) == "new_manager"
+            elif department == 2:
+                assert manager_ctrl.create_new_user(session=session) == "new_seller"
+            elif department == 3:
+                assert manager_ctrl.create_new_user(session=session) == "new_supporter"
