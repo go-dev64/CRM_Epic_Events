e576613577a3eb030c6bdeeae3780d43f12b5499
Oblin Guillaume <guillaume.oblin@sfr.fr>
2023-09-20 21:28:09 +0200

    reffactoring

--- crm/controller/manager_controller.py
+++ crm/controller/manager_controller.py
@@ -14,6 +14,15 @@ class ManagerController:
 
     @auth.is_authenticated
     def create_new_element(self, session):
+        """
+        Function redirects to create a new user or a new contract according to the user's choice.
+
+        Args:
+            session (_type_): _description_
+
+        Returns:
+            _type_: create_new_user or create_new_contract functions.
+        """
         while True:
             choice = self.generic_view.select_element_view()
             match choice:
@@ -26,9 +35,17 @@ class ManagerController:
 
     @auth.is_authenticated
     def create_new_user(self, session):
-        print("get info user")
+        """
+        Function of new user's cretaion.
+        According to user's choice, the function willto create a new manager, or new seller or new supporter.
+
+        Args:
+            session (_type_): _description_
+
+        Returns:
+            _type_: A neww instance of Manager class , or Seller class or Supporter class.
+        """
         user_info = self.user_view.get_user_info_view()
-        print("select department")
         department = self.generic_view.select_element_view()
         match department:
             case 1:
@@ -43,7 +60,16 @@ class ManagerController:
 
     @auth.is_authenticated
     def create_new_contract(self, session):
+        """
+        New contract creation function.
+
+        Args:
+            session (_type_): _description_
+
+        Returns:
+            _type_: new instance of Contract class.
+        """
         print("get info user")
         contract_info = self.contract_view.get_info_contract()
-        new_user = session.current_user.create_new_contract(session=session, contract_info=contract_info)
-        return new_user
+        new_contract = session.current_user.create_new_contract(session=session, contract_info=contract_info)
+        return new_contract
