86010646d92185502816ca2dfe90bb3c959d05c6
Oblin Guillaume <guillaume.oblin@sfr.fr>
2023-09-21 10:31:14 +0200

    create generic view: display element list

--- crm/view/generic_view.py
+++ crm/view/generic_view.py
@@ -1,5 +1,9 @@
 class GenericView:
+    def display_element_list(self, list_element):
+        for i in range(len(list_element)):
+            print(f"{i} - {list_element[i]}")
+
     def select_element_view(self, list_element):
-        print(list_element)
-        element_chosen = ""
+        self.display_element_list(list_element=list_element)
+        element_chosen = input
         return element_chosen
